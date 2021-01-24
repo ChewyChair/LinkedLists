c26f441295b9e2ec35eb6978b8d7c24d58190424
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2021-01-25 00:13:16 +0800

    Second Version

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -18,10 +18,20 @@ public:
 	void printval() {
 		cout << "Current value in account is " << balance << endl;
 	}
+	int num;
 	bankAccount* next;
 private:
 	double balance;
 };
+
+void printAccounts(bankAccount* acct) {
+	cout << "Current bank account numbers: ";
+	while (acct != NULL) {
+		cout << acct->num << " ";
+		cout << endl;
+		acct = acct->next;
+	}
+}
 
 int main() {
 	bankAccount* head = NULL;
@@ -33,12 +43,16 @@ int main() {
 	two = new bankAccount();
 	tail = new bankAccount();
 	head->initialise(0);
+	head->num = 0;
 	head->next = one;
 	one->initialise(100);
+	one->num = 1;
 	one->next = two;
 	two->initialise(50);
+	two->num = 2;
 	two->next = tail;
 	tail->initialise(0);
+	tail->num = 10000;
 	tail->next = NULL;
 	cout << "All accounts initialised. Checking balance of account one: " << endl;
 	one->printval();
@@ -49,4 +63,13 @@ int main() {
 	cout << "Now we add $50 through the public transact() function." << endl;
 	one->transact(50);
 	one->printval();
+	printAccounts(head);
+	cout << "Now we will initialise a third bank account and place it after two and before the tail." << endl;
+	bankAccount* three = NULL;
+	three = new bankAccount();
+	three->initialise(990);
+	three->num = 3;
+	three->next = tail;
+	two->next = three;
+	printAccounts(head);
 }
